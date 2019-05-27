fb465a787e795f9f147657f699b4814401e50cb8
xia-fei <xiafei.dev@gmail.com>
2019-05-27 11:20:26 +0800

    第一次提交

---
+++ mocker-core/src/test/java/org/wing/mocker/core/test/Dog.java
@@ -0,0 +1,22 @@
+package org.wing.mocker.core.test;
+
+public class Dog{
+    private String name;
+    private Cat cat;
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
