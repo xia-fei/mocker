8f8967c2e16dd43bdc780a08ec05d496038e9876
xia-fei <xiafei.dev@gmail.com>
2020-02-16 23:55:12 +0800

    代码 改名重构

---
+++ mock-annotation/src/main/java/org/xiafei/mocke/annotation/MockValue.java
@@ -0,0 +1,13 @@
+package org.xiafei.mocke.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, FIELD, METHOD})
+@Retention(RUNTIME)
+public @interface MockValue {
+    String[] value() ;
+}
